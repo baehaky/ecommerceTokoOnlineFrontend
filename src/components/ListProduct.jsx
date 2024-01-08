5fba41d3509f3257ce557c9bcb366f98ec05e306
baehaky <baehaky1504@gmail.com>
2024-01-09 04:02:32 +0700

    modified:   src/components/ListProduct.jsx 	modified:   src/pages/IndexPage.jsx 	modified:   src/components/ListProduct.jsx 	modified:   src/components/NavigationBar.jsx 	modified:   src/pages/AboutPage.jsx 	modified:   src/pages/IndexPage.jsx 	modified:   tailwind.config.js 	src/assets/brownies.jpg 	src/assets/cheesestick.jpg 	src/assets/putrihomecooking.png 	src/assets/sagon_bakar_samping.jpg 	src/components/Footer.jsx

--- src/components/ListProduct.jsx
+++ src/components/ListProduct.jsx
@@ -2,27 +2,27 @@ export default function ListProduct() {
   return (
     <section className="">
       <h1>Our Product</h1>
-      <div class="flex min-h-screen flex-wrap items-center justify-center">
-        <div class="mx-5">
-          <div class="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
+      <div className="flex min-h-screen flex-wrap items-center justify-center">
+        <div className="mx-5">
+          <div className="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
             <img
-              class="w-full rounded-lg object-cover object-center"
+              className="w-full rounded-lg object-cover object-center"
               src="https://images.unsplash.com/photo-1511556532299-8f662fc26c06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="product"
             />
-            <p class="my-4 pl-4 font-bold text-gray-500">Product Name</p>
-            <p class="mb-4 ml-4 text-xl font-semibold text-gray-800">$399</p>
+            <p className="my-4 pl-4 font-bold text-gray-500">Product Name</p>
+            <p className="mb-4 ml-4 text-xl font-semibold text-gray-800">$399</p>
           </div>
         </div>
-        <div class="mx-5">
-          <div class="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
+        <div className="mx-5">
+          <div className="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
             <img
-              class="w-full rounded-lg object-cover object-center"
+              className="w-full rounded-lg object-cover object-center"
               src="https://images.unsplash.com/photo-1511556532299-8f662fc26c06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="product"
             />
-            <p class="my-4 pl-4 font-bold text-gray-500">Product Name</p>
-            <p class="mb-4 ml-4 text-xl font-semibold text-gray-800">$399</p>
+            <p className="my-4 pl-4 font-bold text-gray-500">Product Name</p>
+            <p className="mb-4 ml-4 text-xl font-semibold text-gray-800">$399</p>
           </div>
         </div>
       </div>
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -21,26 +21,36 @@ export default function IndexPage() {
     },
   };
   return (
-    <section className="bg-gray-100">
+    <section className="bg-gray-100 mt-20 px-10">
       <Carousel responsive={responsive}>
         <div className="flex justify-center items-center">
-          <div className="overflow-hidden ">
+          <div className="overflow-hidden h-auto sm:h-96">
             <img
-              class="w-full"
+              className="w-full"
               src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=1999&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt=""
             />
           </div>
           <div className="absolute flex flex-col text-center justify-center z-30 gap-y-5">
-            <h1 className="text-3xl text-white underline ">Android Smartwatch</h1>
+            <h1 className="text-xl sm:text-3xl text-white shadow-xl uppercase underline ">
+              Android Smartwatch
+            </h1>
+            <p>testting</p>
           </div>
         </div>
-        <div className="flex justify-center bg-gray-100 p-5">
-          <img
-            class="object-cover h-48 w-96"
-            src="https://images.unsplash.com/photo-1511556532299-8f662fc26c06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-          />
+        <div className="flex justify-center items-center">
+          <div className="overflow-hidden h-auto sm:h-96">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1511556532299-8f662fc26c06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </div>
+          <div className="absolute flex flex-col text-center justify-center z-30 gap-y-5">
+            <h1 className="text-3xl text-white underline ">
+              Android Smartwatch
+            </h1>
+          </div>
         </div>
       </Carousel>
       <ListProduct />
